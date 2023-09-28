146f7f7bfc85f189aeeca63d659030b92cfb2df7
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-09-28 12:47:14 +0530

    28-9-23

--- test/todo/index.js
+++ test/todo/index.js
@@ -4,16 +4,52 @@
 // A checkbox to check the item off the list. (checked item should stay in the list, with some differentiation from rest of the todo items)
 // Total number of tasks in the list.
 // Extra points for creativity (think about user accessibility, some animations and things like that)
-  
 
-
-// all variables
 const tasks = [];
-var taskCount;
-var writeTask =document.getElementById
-// var addTasks;
+const input = document.querySelector('input[type="text"]');
+const add = document.querySelector("button");
+const allTasks = document.getElementById("allTaskCount");
+const done = document.getElementById("completedTask");
+const deleteTask = document.querySelector("li a");
+const check = document.querySelector('input[type="radio"]');
 
+// for taking inputs from users
+function inputTask(e) {
+  console.log(input.value);
+  if (e.key === "enter") {
+    const text = e.target.value;
+    const task = {
+      text,
+      id: Date.now().toString(),
+      done: false,
+    };
+    e.target.value = " ";
+    addTasks(task);
+  }
+}
 
+function addTasks(taskId) {
+  const newTasks = tasks.filter(function (task) {
+    return task.id !== taskId;
+  });
 
+  tasks = newTasks;
+}
 
+// for deleting a tasks.
+function deleteTasks() {}
 
+// when user will click on the radio button the task will add in completed task list
+function completedTasks() {}
+
+// here We will take care of alltaskcount
+function allTasksCount() {}
+
+// here we will take care about completed tasks counts
+function completedTasksCount() {}
+
+//
+function showNotification(text) {
+  alert(text);
+}
+add.addEventListener("click", inputTask);
